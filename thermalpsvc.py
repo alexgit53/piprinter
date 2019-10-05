55fd0f94c58e6511a4a20357b4653b5d6509ffec
Alex Morrison <alexkmorrison@gmail.com>
2019-10-05 23:45:49 +0100

    Tidy up message and increase bounce window to try and prevent double print

--- thermalpsvc.py
+++ thermalpsvc.py
@@ -19,6 +19,7 @@ def get_day_section(hour):
         day_section = "afternoon"
     else:
         day_section = "evening"
+    return day_section
 
 
 class PrintSvc:
@@ -38,7 +39,7 @@ class PrintSvc:
 
     def run(self):
         GPIO.remove_event_detect(self.button_pin)
-        GPIO.add_event_detect(self.button_pin, GPIO.FALLING, bouncetime=200)
+        GPIO.add_event_detect(self.button_pin, GPIO.FALLING, bouncetime=500)
         GPIO.add_event_callback(self.button_pin, self._print_handler)
         while True:
             sleep(0.01)
@@ -58,11 +59,10 @@ class PrintSvc:
             post = get_cat_post()
             now_dt = datetime.now()
             day_section = get_day_section(now_dt.hour)
-            self.printer.out("Hello! Here's a picture of a cat...")
+            self.printer.out("Hello! Here's a cat picture")
             self.printer.image(self._scaled_image(post["pic"]))
             self.printer.out(post["title"])
-            self.printer.out(post["link"])
-            self.printer.out(f"Have a good {day_section}.")
+            self.printer.out(f"Have a good {day_section}!")
             self.printer.feed(3)
         finally:
             self._set_led(False)
