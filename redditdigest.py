1935331d24d78c4763eec1cf0d7c0f16f1a570c4
Alex Morrison <alexkmorrison@gmail.com>
2019-10-05 23:54:03 +0100

    Switch to random post from whole sub instead of top 10 for the day

--- redditdigest.py
+++ redditdigest.py
@@ -30,25 +30,21 @@ def get_image(url):
             return i
         else:
             raise DownloadError("Download failed")
-            
+
 
 def get_cat_post():
     logging.debug("Fetching cat post")
     reddit = praw.Reddit("bot1", user_agent="pc:com.ozymandias.redditdigest:v1 (by /u/CoachOzymandias)")
     image_posts = []
-    for post in reddit.subreddit('cats').top("day", limit=50):
-        if is_image_post(post) and not post.over_18:
-            image_posts.append(post)
-        if len(image_posts) >= 10:
-            break
-
+    post = reddit.subreddit('cats').random()
+    while not is_image_post(post) or post.over_18:
+        post = reddit.subreddit('cats').random()
     tries = 0
     while tries < MAX_DOWNLOAD_TRIES:
-        choice = random.choice(image_posts)
         try:
-            cat_pic = get_image(choice.url)
-            logging.info(f"Selected {choice.permalink}")
-            return {"pic": cat_pic, "title": choice.title, "link":choice.permalink}
+            cat_pic = get_image(post.url)
+            logging.info(f"Selected {post.permalink}")
+            return {"pic": cat_pic, "title": post.title, "link":post.permalink}
         except DownloadError:
             logging.debug(f"Download failed on try {tries}/{MAX_DOWNLOAD_TRIES}.")
             tries += 1
